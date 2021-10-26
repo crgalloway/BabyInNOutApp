4fbee6e13b881459e5b3e6a2e74eb1290cc186b3
Craig Galloway <crgalloway@gmail.com>
2021-10-26 14:17:13 -0700

    Added an ugly but visible log

--- Screens/Log/LogScreen.js
+++ Screens/Log/LogScreen.js
@@ -1,36 +1,102 @@
 import React, { useState, useEffect } from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import ButtonLink from "../../components/ButtonLink";
 import styles from "../../Base.StyleSheet";
+import moment from "moment";
 
-var display = [{ type: "Dummy", id: "asdf" }];
+var display = { dates: [] };
 
 const LogScreen = (props) => {
   const [loaded, setLoaded] = useState(false);
+
   useEffect(() => {
     var apiURL = "http://localhost:8000/api/get-all/1"; //Hard-coding until there's more than 1 baby
     if (!loaded) {
       fetch(apiURL, { method: "GET" })
         .then((response) => response.json())
         .then((data) => {
-          console.log("data");
-
-          // display = [];
-          // display = display.concat(data);
-          console.log(data);
           display = data;
           setLoaded(true);
-          // setDisplay(data);
         });
     }
   });
 
   return (
     <View style={styles.container}>
-      <ButtonLink text="close" />
-      {display.map((item) => {
-        return <Text key={item.id}>FOO {item.type}</Text>;
+      {display.dates.map((date) => {
+        let viewDate = (
+          <View>
+            <Text key={date}>{date}</Text>
+            {display[date].map((item) => {
+              let viewItem;
+              let comment;
+              if (item.comment && item.comment.length) {
+                console.log(item.comment);
+                comment = <Text>Comment: {item.comment}</Text>;
+              }
+              if (item.dataType === "feeding") {
+                viewItem = (
+                  <View
+                    key={item.id}
+                    style={{
+                      borderBottomColor: "black",
+                      borderBottomWidth: 1,
+                    }}
+                  >
+                    <Text>
+                      {moment(item.time).format("h:mm a")} | Feeding |{" "}
+                      {item.quantity} oz | {item.type}
+                    </Text>
+                    {comment}
+                  </View>
+                );
+              } else if (item.dataType === "nap") {
+                viewItem = (
+                  <View
+                    key={item.id}
+                    style={{
+                      borderBottomColor: "black",
+                      borderBottomWidth: 1,
+                    }}
+                  >
+                    <Text>
+                      {moment(item.time).format("h:mm a")} | Sleep |{" "}
+                      {item.startTime
+                        ? moment(item.startTime).format("h:mm a")
+                        : "?"}{" "}
+                      |{" "}
+                      {item.endTime
+                        ? moment(item.endTime).format("h:mm a")
+                        : "?"}
+                    </Text>
+                  </View>
+                );
+              } else if (item.dataType === "diaper") {
+                viewItem = (
+                  <View
+                    key={item.id}
+                    style={{
+                      borderBottomColor: "black",
+                      borderBottomWidth: 1,
+                    }}
+                  >
+                    <Text>
+                      {moment(item.time).format("h:mm a")} | Diaper |{" "}
+                      {item.urineAmount} Urine | {item.stoolAmount} Stool
+                    </Text>
+                    {comment}
+                  </View>
+                );
+              } else {
+                viewItem = <Text key={item.id}>FOO {item.dataType}</Text>;
+              }
+              return viewItem;
+            })}
+          </View>
+        );
+        return viewDate;
       })}
+      <ButtonLink text="close" />
     </View>
   );
 };
